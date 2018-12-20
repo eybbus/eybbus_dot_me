65034ff8422fd99665f04ef96caf2637b9d42bd1
eybbus <eythorfreyr94@gmail.com>
2018-12-20 03:40:43 +0000

    Changed siteMetadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
-    description: 'Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.',
-    author: '@gatsbyjs',
+    title: 'Eyþór Freyr Óskarsson',
+    description: 'Eyþór Freyr Óskarsson portfolio',
+    author: '@eybbus',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
